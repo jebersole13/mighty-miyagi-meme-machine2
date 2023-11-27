ef58edb91156e436ae591bb803be2be2e568f444
adawg2010 <jcebersole1@gmail.com>
2023-11-27 22:08:39 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <h1 className="text-8xl font-bold ">
+      Welcome to the Mighty Miyagi Meme Machine!
+    </h1>
+    <br />
+<img src="miyagiyes.gif" alt="yes" />
+      </div>
+     
+    </>
+  )
+}
+
+export default App
