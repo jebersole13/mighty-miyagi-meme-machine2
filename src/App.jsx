a917b9eecd7a4fd2f17e1c6ac856cebef58d731c
adawg2010 <jcebersole1@gmail.com>
2023-11-27 22:45:27 +0900

    asis

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ function App() {
   return (
     <>
       <div>
-      <h1 className="text-8xl font-bold ">
+      <h1 className="text-blue-800 text-9xl ">
       Welcome to the Mighty Miyagi Meme Machine!
     </h1>
     <br />
-<img src="miyagiyes.gif" alt="yes" />
+<img className='object-none object-center' src="miyagiyes.gif" alt="yes" />
+<br />
+<br />
+<button className='rounded-full'>Start</button>
+
       </div>
      
     </>
