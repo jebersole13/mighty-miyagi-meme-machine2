c103ac74cf63dcd33d858b70e83594a5740a784a
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-11-28 14:42:29 +0900

    Headeradd

---
+++ src/MiyagiHeader.jsx
@@ -0,0 +1,17 @@
+function MiyagiHeader(){
+    return(
+<div>
+<h1 className="text-white-800 text-9xl ">
+      Welcome to the Mighty Miyagi Meme Machine!
+    </h1>
+    <h3>Your destination for things Miygai fact and fictional</h3>
+    <br />
+<img className=' float-left' src="miyagiyes.gif" alt="yes" id='miyagi' /> 
+<img className='float-right' src="final-miyagi.jpg" alt="rm" />
+<br />
+<br />
+</div>
+    )
+}
+
+export default  MiyagiHeader
