c9947d5b0d14b3b95899273c2bf130cb726d0805
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-11-28 15:53:36 +0900

    32resize

--- src/MiyagiHeader.jsx
+++ src/MiyagiHeader.jsx
@@ -7,8 +7,8 @@ function MiyagiHeader(){
     <br />
     <h3>Your destination for things Miygai fact and fictional</h3>
     <br />
-<img className=' float-left h-24 w-24 overflow-hidden' src="miyagiyes.gif" alt="yes" id='miyagi' /> 
-<img className=' float-right  h-24 w-24  overflow-hidden' src="final-miyagi.jpg" alt="rm" />
+<img className=' float-left h-32 w-32 overflow-hidden' src="miyagiyes.gif" alt="yes" id='miyagi' /> 
+<img className=' float-right  h-32 w-32  overflow-hidden' src="final-miyagi.jpg" alt="rm" />
 <br />
 <br />
 </div>
