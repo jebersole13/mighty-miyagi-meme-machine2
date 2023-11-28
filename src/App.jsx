a244ed99afbb2597b58f52f930e49fbd7ec80520
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2023-11-28 14:04:10 +0900

    pic fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,10 @@ function App() {
       <h1 className="text-blue-800 text-9xl ">
       Welcome to the Mighty Miyagi Meme Machine!
     </h1>
+    <h3>Your destination for things Miygai fact and fictional</h3>
     <br />
 <img className=' float-left' src="miyagiyes.gif" alt="yes" id='miyagi' /> 
-<img className='float-right' src="final-miyagi" alt="rm" />
+<img className='float-right' src="final-miyagi.jpg" alt="rm" />
 <br />
 <br />
 <button className='rounded-full shadow hover:shadow-lg'>Start</button>
